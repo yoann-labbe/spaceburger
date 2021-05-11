b2b59d8174f40172441a78784c047358077345ec
yoann189.labbe@laposte.net <y.labbe.contact@gmail.comgit log -pwild-gitls$ lspwd~cdgit config --global user.email y.labbe.contact@gmail.com>
2021-05-12 01:50:41 +0200

    Router et Main

--- src/App.js
+++ src/App.js
@@ -4,15 +4,32 @@ import Footer from "./Layout/Main/Footer/Footer";
 import Header from "./Layout/Main/Header/Header";
 import HomeContent from "./Layout/Main/HomeContent/HomeContent";
 import Menu from "./View/Components/Menu";
-
+import Load from "./View/Components/Load";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Main from "./Layout/Main/Main";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      {/*<HomeContent />*/}
-      <Menu />
-      <Footer />
+    <div>
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <Main>
+                <HomeContent />
+              </Main>
+            )}
+          />
+
+          <Header />
+
+          {/*<Menu />*/}
+          <Load />
+          <Footer />
+        </Switch>
+      </Router>
     </div>
   );
 }
