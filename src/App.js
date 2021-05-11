d1e66a8d0a7e4598fe68141b2df2cf105e9325ae
Adele <aguessya@yahoo.fr>
2021-05-12 01:47:51 +0200

    ajout de la page MenuProduits

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Footer from "./Layout/Main/Footer/Footer";
 import Header from "./Layout/Main/Header/Header";
 import HomeContent from "./Layout/Main/HomeContent/HomeContent";
 import Menu from "./View/Components/Menu";
+import MenuProduits from "./View/Components/MenuProduits";
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/*<HomeContent />*/}
-      <Menu />
+      {/* <HomeContent /> */}
+      <MenuProduits />
+      {/* <Menu /> */}
       <Footer />
     </div>
   );
