1e65efedde5ba39542f4dbd3c0af9879731a76f9
Esther241 <esther.dinh@gmail.com>
2021-05-12 01:06:41 +0200

    resto + card

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Footer from "./Layout/Main/Footer/Footer";
 import Header from "./Layout/Main/Header/Header";
 import Restaurant from "./Views/Components/RestaurantList/Restaurant";
 import HomeContent from "./Layout/Main/HomeContent/HomeContent";
+import ProductsLists from "./Views/Components/Products/ProductsLists";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Restaurant />
+      {/* <Restaurant /> */}
+      <ProductsLists />
       {/* <HomeContent /> */}
       <Footer />
     </div>
---
+++ src/Views/Components/Products/ProductsLists.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MediaCard from "./CardProducts";
+import CenteredTabs from "./Products";
+
+const ProductsLists = () => {
+  return (
+    <div>
+      <CenteredTabs />
+      <MediaCard />
+    </div>
+  );
+};
+
+export default ProductsLists;
