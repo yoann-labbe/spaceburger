b8fd688a1b8f7bf2fc1c24801c211571fa6288e0
Adele <aguessya@yahoo.fr>
2021-05-11 14:36:10 +0200

    creation contenu page Accueil

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from "react";
 import "./App.css";
 import Header from "./Layout/Main/Header/Header";
+import HomeContent from "./Layout/Main/HomeContent/HomeContent";
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomeContent />
     </div>
   );
 }
