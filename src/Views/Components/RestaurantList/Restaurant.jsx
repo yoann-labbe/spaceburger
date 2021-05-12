945771f2498ed9389d84b38c35e674b466a028ac
yoann189.labbe@laposte.net <y.labbe.contact@gmail.comgit log -pwild-gitls$ lspwd~cdgit config --global user.email y.labbe.contact@gmail.com>
2021-05-12 14:45:07 +0200

    modif encore

--- src/Views/Components/RestaurantList/Restaurant.jsx
+++ src/Views/Components/RestaurantList/Restaurant.jsx
@@ -4,20 +4,33 @@ import InsetList from "./InsetList";
 
 const useStyles = makeStyles((theme) => ({
   container: {
+
     justifyContent: "center",
   },
+  position: {
+    display: "flex",
+    justifyContent:"center",
+  },
+  title: {
+    display: "flex",
+    justifyContent:"center",
+  }
 }));
 
 const Restaurant = () => {
 const classes = useStyles();
   return (
     <div className={classes.container}>
+      <div className={classes.title}>
       <h2>Trouver un restaurant </h2>
+      </div>
+      <div className={classes.position}>
       <img
         src="assets/mars2.jpg"
         alt="map.location"
-        style={{ maxWidth: "400px" }}
+        style={{ maxWidth: "400px", }}
       />
+      </div>
       <InsetList />
     </div>
   );
